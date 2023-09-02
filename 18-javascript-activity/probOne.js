b5443f5621a3306c82c35b6babfafa43c785c8ca
John Farinas <jfaris09@gmail.com>
2023-09-02 09:43:21 +0800

    Update

---
+++ 18-javascript-activity/probOne.js
@@ -0,0 +1,15 @@
+//Problem #1
+let balance = 1000;
+
+const transaction1 = 500;
+const transaction2 = -200;
+const transaction3 = -1000;
+
+balance += transaction1;
+console.log("Final balance:", balance);
+
+balance += transaction2;
+console.log("Final balance:", balance);
+
+balance += transaction3;
+console.log("Final balance:", balance);
