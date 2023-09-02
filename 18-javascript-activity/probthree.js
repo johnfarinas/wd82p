f097ab886edd9194159f4a84f8c54d071204fd43
John Farinas <jfaris09@gmail.com>
2023-09-02 09:49:46 +0800

    Update

---
+++ 18-javascript-activity/probthree.js
@@ -0,0 +1,9 @@
+const temperature = 25; 
+
+if (temperature > 30) {
+  console.log("It's hot outside!");
+} else if (temperature >= 20 && temperature <= 30) {
+  console.log("The weather is pleasant.");
+} else {
+  console.log("It's a bit chilly.");
+}
---
+++ 18-javascript-activity/probtwo.js
@@ -0,0 +1,9 @@
+const testScore1 = 85;
+const testScore2 = 92;
+const testScore3 = 78;
+
+const averageScore = (testScore1 + testScore2 + testScore3) / 3;
+
+const roundedAverageScore = Math.round(averageScore);
+
+console.log("Average Test Score:", roundedAverageScore);
