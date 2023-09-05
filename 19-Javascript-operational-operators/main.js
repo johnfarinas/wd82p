ee7292a7278f4ff36ce5196c694d431022245e7f
John Farinas <jfaris09@gmail.com>
2023-09-05 19:20:35 +0800

    Updated

---
+++ 19-Javascript-operational-operators/main.js
@@ -0,0 +1,21 @@
+// const subtractNum = (a, b) => a - b;
+
+// console.log(subtractNum(15, 80));
+
+
+// function subtractNum( a, b ) {
+// console.log(a-b);
+// }
+// subtractNum( 80, 27 );
+
+// }
+
+function fullname(first, last) {
+
+return first + " " + last;
+
+}
+
+
+console.log(fullname("Carlo", "Dacuyan"));
+console.log(fullname("Bruce", "Lee"));
